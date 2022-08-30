ff5beed4d5ee9c83047af6b220dcde6087fe42c2
Roman <coderc770@gmail.com>
2022-08-30 17:41:57 +0300

    Added procfile and runtime.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taxi-service-2022.wsgi --log-file -
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -40,14 +40,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "crispy_forms",
     "taxi",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
