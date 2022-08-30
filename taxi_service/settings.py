ed2c10c49544c1c0b4cfaa731b81dccd8f2f52f1
Roman <coderc770@gmail.com>
2022-08-30 18:04:39 +0300

    modifed settings

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-# SECURITY WARNING: don"t run with debug turned on in production!
+# SECURITY WARNING: don"t run with debug turned on in production!!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "FALSE"
 
 ALLOWED_HOSTS = ["127.0.0.1", "taxi-service-2022.herokuapp.com"]
